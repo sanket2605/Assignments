cd22e383ea73ad74daceee35d4d37629996cd597
Sanket <mores2998@gmail.com>
2023-10-23 19:28:19 +0530

    Create Calculator.cs

---
+++ DotNET/Day1Assignment/Calculator.cs
@@ -0,0 +1,22 @@
+namespace ConsoleD1
+{
+    internal class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Product(int x, int y) 
+        {
+            return x * y;
+        }
+        static void Main(string[] args)
+        {
+           
+           Calculator c1 = new Calculator();
+            int sum=c1.Add(4, 5);
+            int prod=c1.Product(4, 5);
+            Console.WriteLine("sum is {0} and product is {1}", sum, prod);
+        }
+    }
+}
