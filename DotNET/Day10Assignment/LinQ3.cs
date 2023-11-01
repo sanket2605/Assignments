b900047f3061f65afdd034eb4a5b2842e1b51f18
SanketMore26 <mores2998@gmail.com>
2023-11-01 19:29:09 +0530

    Create LinQ3.cs

---
+++ DotNET/Day10Assignment/LinQ3.cs
@@ -0,0 +1,23 @@
+//Q.int[] a={5,8,9,2,1}; display all Even numbers
+
+namespace Day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 5, 8, 9, 2, 1 };
+             
+            var subset= from i in a where i%2==1 select i;
+            foreach (var item in subset)
+            {
+                Console.WriteLine(item);
+            }
+            var subset1=a.Where(i => i%2==1).Select(i => i);
+            foreach (var item in subset1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
