276097860c65a07fe389f38805a941dcea18efe7
SanketMore26 <mores2998@gmail.com>
2023-11-01 19:34:12 +0530

    Update LinQ2.cs

--- DotNET/Day10Assignment/LinQ2.cs
+++ DotNET/Day10Assignment/LinQ2.cs
@@ -2,17 +2,19 @@
 
 namespace Day10
 {
-    internal class Program
+    internal class LinQ2
     {
         static void Main(string[] args)
         {
             int[] a = { 5, 8, 9, 2, 1 };
-             
+
+            //using LinQ
             var subset= from i in a where i%2==0 select i;
             foreach (var item in subset)
             {
                 Console.WriteLine(item);
             }
+            //using Lambda
             var subset1=a.Where(i => i%2==0).Select(i => i);
             foreach (var item in subset1)
             {
