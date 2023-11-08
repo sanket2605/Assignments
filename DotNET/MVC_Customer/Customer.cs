4b92e681d4f6c4bd01b8f24fc07cde959dafef00
SanketMore26 <mores2998@gmail.com>
2023-11-08 19:51:36 +0530

    Update and rename DotNET/WB_Customer.sln to DotNET/MVC_Customer/Customer.cs

---
+++ DotNET/MVC_Customer/Customer.cs
@@ -0,0 +1,13 @@
+namespace WB_Customer.Models
+{
+    public class Customer
+    {
+
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int MobileNo { get; set; }
+        public int BillAmount { get; set; }
+
+    }
+}
+
