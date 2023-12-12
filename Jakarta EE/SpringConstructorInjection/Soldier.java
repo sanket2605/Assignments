ff26d7b1f46858f9aa78bb5ffc91297b511368b5
SanketMore26 <mores2998@gmail.com>
2023-12-12 20:14:08 +0530

    Update Soldier.java

--- Jakarta EE/SpringConstructorInjection/Soldier.java
+++ Jakarta EE/SpringConstructorInjection/Soldier.java
@@ -2,13 +2,9 @@ package mypack;
 public class Soldier
 {
     private Weapon weapon;
-    public Soldier()
-    {
-        System.out.println("Soldier no-arg constructor");
-    }
+    
     public void setweapon(Weapon weapon) 
     {
-        System.out.println("inside setter method");
         this.weapon = weapon;
     }  
     public void perform()
